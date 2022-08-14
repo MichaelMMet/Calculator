a22b913d4423d7f32e8da30b206c2bb77282b715
Michael Metcalf <mikegmac14@gmail.com>
2022-08-14 14:51:06 -0700

    Add basic calculator UI

--- index.js
+++ index.js
@@ -1,27 +1,47 @@
-function add(a, b){
-return a + b;
+function add(a, b) {
+    return a + b;
+}
+
+
+function subtract(a, b) {
+    return a - b;
+}
+
+
+
+function multiply(a, b) {
+    ; let sum = a * b;
+    return sum;
+}
+
+
+function divide(firstNum, secondNum) {
+    return firstNum / secondNum;
+}
+
+
+function operate(operator, numOne, numTwo) {
+    switch (operator) {
+        case "+":
+            return add(numOne, numTwo);
+            break;
+
+        case "-":
+            subtract(numOne, numTwo);
+            break;
+
+        case "*":
+            return multiply(numOne, numTwo);
+            break;
+
+        case "/":
+            divide(numOne, numTwo);
+            break;
+    }
 
 }
 
-console.log(add(5, 876));
+console.log(operate("*", 7, 5));
 
-function subtract(a, b){
-return a - b;
-}
+console.log(operate("+", 7, 5));
 
-console.log(subtract(7, 3));
-
-
-function multiply(a, b){
-return a * b;
-}
-
-console.log(multiply(6, 10));
-
-function divide(firstNum, secondNum){
-return firstNum / secondNum;
-}
-
-console.log(divide(89, 4));
-
-operat
