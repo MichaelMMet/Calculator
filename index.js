ebd72ac142764c929f3436efd2bbea7df33b49e6
Michael Metcalf <mikegmac14@gmail.com>
2022-08-14 14:09:14 -0700

    Add basic add, subtract, multiply, and divide functions

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a, b){
+return a + b;
+
+}
+
+console.log(add(5, 876));
+
+function subtract(a, b){
+return a - b;
+}
+
+console.log(subtract(7, 3));
+
+
+function multiply(a, b){
+return a * b;
+}
+
+console.log(multiply(6, 10));
+
+function divide(firstNum, secondNum){
+return firstNum / secondNum;
+}
+
+console.log(divide(89, 4));
+
+operat
